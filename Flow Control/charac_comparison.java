0c5a70a747fd0210c3aff61c514d6b4c2cb91563
Anushka-deokar <deokaranushka@gmail.com>
2025-08-01 15:13:29 +0530

    Loops

---
+++ Flow Control/charac_comparison.java
@@ -0,0 +1,12 @@
+
+public class charac_comparison {
+    public static void main(String args[]) {
+        char ch = 'A';
+        if (Character.isUpperCase(ch)) {
+            System.out.println("Upppercase");
+        } else {
+            System.out.println("LowerCase");
+        }
+    }
+
+}
