1acbe65269afe73822a06ea0aa329db0f7d59a9b
Anushka-deokar <deokaranushka@gmail.com>
2025-08-01 14:01:30 +0530

    sum of 2 numbers

--- sum_of_integers.java
+++ sum_of_integers.java
@@ -1,3 +1,11 @@
-public class sum_of_integers {
+import java.util.Scanner;
 
+public class sum_of_integers {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        System.out.println("Sum is " + (a + b));
+
+    }
 }
