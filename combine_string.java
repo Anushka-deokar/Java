64bd425ae82b39acb61acfbfe38dfedeb01270c7
Anushka-deokar <deokaranushka@gmail.com>
2025-08-01 12:23:55 +0530

    string-integers

---
+++ combine_string.java
@@ -0,0 +1,9 @@
+public class combine_string {
+    public static void main(String args[]) {
+        String s1 = "Anushka";
+        String s2 = "Deokar ";
+        String concat_string = s1 + " " + s2;
+        System.out.println(concat_string);
+    }
+
+}
---
+++ second.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class second {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Name : ");
+        String name = sc.nextLine();
+        System.out.println("Enter your age : ");
+        int age = sc.nextInt();
+        System.out.println("Your name is " + name + "\nYour Age is : " + age);
+    }
+
+}
