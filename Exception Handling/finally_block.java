5672bc12760771bdec1b446f12c57f7a49fd3d4f
Anushka-deokar <deokaranushka@gmail.com>
2025-08-04 17:31:58 +0530

    exception-handling

---
+++ Exception Handling/finally_block.java
@@ -0,0 +1,11 @@
+public class finally_block {
+    public static void main(String[] args) {
+        try {
+            int a = 5 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Exception caught");
+        } finally {
+            System.out.println("This always executes");
+        }
+    }
+}
