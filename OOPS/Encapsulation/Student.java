35539aa734e7ec96f43f82bebcf5a652d9f058e8
Anushka-deokar <deokaranushka@gmail.com>
2025-08-06 17:26:22 +0530

    encapsulation

---
+++ OOPS/Encapsulation/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+  private String name;
+  private int age;
+
+  // getter for name
+  public String getName() {
+    return name;
+  }
+
+  // setter for name
+  public void setName(String newName) {
+    name = newName;
+  }
+
+  // getter for age
+  public int getAge() {
+    return age;
+  }
+
+  // setter for age
+  public void setAge(int newAge) {
+    age = newAge;
+  }
+}
---
+++ OOPS/Encapsulation/first.java
@@ -0,0 +1,11 @@
+public class first {
+    public static void main(String args[]) {
+        Student s = new Student();
+        s.setName("Anushka");
+        s.setAge(22);
+
+        System.out.println("Name is : " + s.getName());
+        System.out.println("Age is : " + s.getAge());
+
+    }
+}
