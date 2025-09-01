0fe8600e338796c567c7234681bf1ec273c5d9b8
Anushka-deokar <deokaranushka@gmail.com>
2025-09-01 15:04:45 +0530

    arrays

---
+++ Arrays/input_output.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class input_output {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5 numbers : ");
+        for (int i = 0; i <= arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array Elements are : ");
+        for (int num : arr) {
+            System.out.println(num + " ");
+        }
+    }
+
+}
