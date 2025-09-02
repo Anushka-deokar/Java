234bb0eb2d8693569ce005ae152bb52f5a2b68d6
Anushka-deokar <deokaranushka@gmail.com>
2025-09-02 22:12:38 +0530

    oops

---
+++ OOPS/Polymorphism/method_overloading.java
@@ -0,0 +1,5 @@
+//Method Overloading: When a class has multiple methods with the same name but different parameters (different type, number, or both), it is known as method overloading. The correct method is chosen at compile time based on the arguments passed.
+
+public class method_overloading {
+
+}
