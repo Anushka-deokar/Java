e37e1da677d67f4c920bb23dd6af1154fa36f4a0
Anushka-deokar <deokaranushka@gmail.com>
2025-09-11 08:15:40 +0530

    ArrayList

---
+++ Collections/ArrayList/ArrayLists.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+class ArrayLists{
+    public static void main(String args[])
+    {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+
+        //add elements using .add()
+        list.add(0);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+
+        //get elements using .get() we need to pass index
+
+        int element = list.get(0);
+        System.out.println(element);
+
+
+
+    }
+
+}
